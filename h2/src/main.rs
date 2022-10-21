c6cb92cb904630217dd9370ec0aefa068047991d
WhiteBlackGoose <wbg@angouri.org>
2022-10-21 08:49:01 +0300

    u

--- h2/src/main.rs
+++ h2/src/main.rs
@@ -1,29 +1,28 @@
 use rand::Rng;
-use std::io;
 use std::cmp::Ordering;
+
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
 
 fn main() {
     println!("Guess the number");
-    let sec = rand::thread_rng().gen_range(1..=1000000);
-    loop {
-        println!("Please input your guess");
-    
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        match guess.cmp(&sec) {
-            Ordering::Less => println!("Too little"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You won");
-                break;
-            },
+    let _sec: i32 = rand::thread_rng().gen_range(1..=1000000);
+    let b = {
+        let a = [1, 2, 3];
+        sum(a[0], a[1])
+    };
+    let result = {
+        let mut i = 0;
+        loop {
+            i += 1;
+            if i > 5 { break i; }
         }
+    };
+    println!("{result}");
+
+    for el in (1..5).rev() {
+        println!("{el}");
     }
 }
