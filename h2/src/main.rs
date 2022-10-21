f781e8b7667495a649e0ca95e4f63e0ea1d96a4c
WhiteBlackGoose <wbg@angouri.org>
2022-10-21 12:05:27 +0300

    smh

--- h2/src/main.rs
+++ h2/src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::cmp::Ordering;
 
+#[derive(Debug)]
 struct User {
     active: bool,
     username: String,
@@ -8,28 +9,41 @@ struct User {
     sign_in_count: u64
 }
 
-struct Color(i32, i32, i32);
-
-fn first_word(s: &str) -> &str {
-    let mut i: usize = 0;
-    for (i, &c) in s.as_bytes().iter().enumerate() {
-        if c == b' ' {
-            return &s[..i];
-        } 
+impl User {
+    fn full_name(&self) -> String {
+        let mut owned = self.username.clone();
+        owned.push_str(&self.email);
+        owned   
     }
-    &s[..]
+    fn dummy(name: String) -> Self {
+        User {
+            active: false,
+            username: name,
+            email: String::new(),
+            sign_in_count: 0
+        }
+    }
+}
+
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+    MyAss { x: i32, y: i32 }
 }
 
 fn main() {
-    let s: String = String::from("Hello world aaa");
-    let w = first_word(&s);
-    let ggg = &s[0..1];
-    println!("{w}");
     let user1 = User {
-        email: String::from("aaa"),
-        username: String::from("aaa"),
+        email: String::from("heh@hehe.com"),
+        username: String::from("goose"),
         active: true,
         sign_in_count: 1
     };
-    user1.email = String::from("aaaa");
+    println!("user1 is {:?}", user1.full_name());
+    let four = IpAddrKind::V4(String::from("aaa"));
+    let six = IpAddrKind::V6(String::from("bbb"));
+    let x = match IpAddrKind::MyAss { x: 32, y: 44 } {
+        IpAddrKind::V4(&s) => String::from("v4 {s}"),
+        IpAddrKind::V6(&s) => String::from("v6 {s}"),
+        IpAddrKind::MyAss{ x: x, y: y} => String::from("my ass {x} {y} ")
+    };
 }
