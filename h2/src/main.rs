cc51b2e8ee8c883d60dc898e1353f44bca368d00
WhiteBlackGoose <wbg@angouri.org>
2022-10-21 09:40:25 +0300

    u

--- h2/src/main.rs
+++ h2/src/main.rs
@@ -1,28 +1,35 @@
 use rand::Rng;
 use std::cmp::Ordering;
 
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64
+}
 
-fn sum(a: i32, b: i32) -> i32 {
-    a + b
+struct Color(i32, i32, i32);
+
+fn first_word(s: &str) -> &str {
+    let mut i: usize = 0;
+    for (i, &c) in s.as_bytes().iter().enumerate() {
+        if c == b' ' {
+            return &s[..i];
+        } 
+    }
+    &s[..]
 }
 
 fn main() {
-    println!("Guess the number");
-    let _sec: i32 = rand::thread_rng().gen_range(1..=1000000);
-    let b = {
-        let a = [1, 2, 3];
-        sum(a[0], a[1])
+    let s: String = String::from("Hello world aaa");
+    let w = first_word(&s);
+    let ggg = &s[0..1];
+    println!("{w}");
+    let user1 = User {
+        email: String::from("aaa"),
+        username: String::from("aaa"),
+        active: true,
+        sign_in_count: 1
     };
-    let result = {
-        let mut i = 0;
-        loop {
-            i += 1;
-            if i > 5 { break i; }
-        }
-    };
-    println!("{result}");
-
-    for el in (1..5).rev() {
-        println!("{el}");
-    }
+    user1.email = String::from("aaaa");
 }
