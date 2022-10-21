4d61595bcd369e60323b2690fe83a173a81426cc
WhiteBlackGoose <wbg@angouri.org>
2022-10-21 19:21:19 +0300

    upd

--- stoch-proc/src/main.rs
+++ stoch-proc/src/main.rs
@@ -5,6 +5,7 @@ use crossterm::{
 use std::io::{stdout, Write};
 use std::vec::Vec;
 use std::{thread, time};
+use rand_distr::Distribution;
 
 struct Ring<T> {
     vec: Vec<T>,
@@ -52,14 +53,15 @@ impl<T> Ring<T> {
     }
 }
 
-fn draw_graph(ring: &Ring<i32>, height: i16, std: &mut std::io::Stdout) {
+
+fn draw_graph(ring: &Ring<f64>, height: i16, std: &mut std::io::Stdout) {
     std.execute(terminal::Clear(terminal::ClearType::All)).unwrap();
-    let min_val = ring.iter().min().unwrap();
-    let max_val = ring.iter().max().unwrap();
+    let min_val = ring.iter().fold(f64::MAX, f64::min);
+    let max_val = ring.iter().fold(f64::MIN, f64::max);
     let range = max_val - min_val;
     for (x, val) in ring.iter().enumerate() {
         let val = val - min_val;
-        let y = val * (height as i32) / range;
+        let y = val * (height as f64) / range;
         std
             .queue(cursor::MoveTo(x as u16, y as u16)).unwrap()
             .queue(style::PrintStyledContent("█".magenta())).unwrap();
@@ -73,12 +75,13 @@ fn main() {
         let (w, h) = term_size::dimensions().unwrap();
         (w as i16, h as i16)
     };
-    let mut ring: Ring<i32> = Ring::new(width as usize, 0);
-    let mut value = 0;
+    let mut ring: Ring<f64> = Ring::new(width as usize, 0.0);
+    let mut value: f64 = 0.0;
+    let normal = rand_distr::Normal::new(0.0, 1.0).unwrap();
     loop {
         thread::sleep(time::Duration::from_millis(300));
 
-        value += if rand::random() { -1 } else { 1 };
+        value += normal.sample(&mut rand::thread_rng());
         ring.push(value);
 
         draw_graph(&ring, height, &mut stdout);
